6904230cac756b4afe3b81f93f6963d945c4406a
lxz <bithub@github.com>
2021-05-22 21:50:04 +0800

    Update git ignore.

--- build/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Config, Network, NetworkId, Target, TargetsRecord, ContractsMap, HorizonJS, Synth, Token } from "./types";
-declare const horizon: ({ networkId, network, signer, provider, }: Config) => HorizonJS;
-export { horizon, Network, NetworkId };
-export type { Config, Target, TargetsRecord, ContractsMap, HorizonJS, Synth, Token, };
-export default horizon;
